6c06f8b8db9f11089f593d9940a270e13998c1b0
Igor Vytyaz <vytyaz@google.com>
2019-01-28 16:13:04 -0800

    Updating copyright headers.

--- pxr/usd/plugin/usdDraco/fileFormat.cpp
+++ pxr/usd/plugin/usdDraco/fileFormat.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2018 Pixar
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "Apache License")
 // with the following modification; you may not use this file except in
